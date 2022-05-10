deaea1ae6bcf677aae30a0c395de8427c9974820
kernbeisser <kernbeisser@tom-bonn.de>
2022-05-10 07:04:38 +0200

    das gerüst steht!

---
+++ src/template.py
@@ -0,0 +1,21 @@
+""" imports  -- irgendwann steht hier etwas
+    Sinnvolles, gelle
+"""
+
+
+class TuWas():
+    """Klasse!"""
+
+    def __init__(self):
+        pass
+
+
+def main():
+    """
+    hier kommt die Sache ins Rollen ...
+    Und irgendwann steht hier etwas Sinnvolles, gelle
+    """
+
+
+if __name__ == " __main__":
+    main()
